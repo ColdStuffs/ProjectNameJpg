68f220084e20589195f7a39c52d42e3423c4e066
ColdStuffs <60492209+ColdStuffs@users.noreply.github.com>
2021-12-08 17:55:19 -0800

    THETHIUNG ITHURTHEAD

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-function createObject(first, last, school, gradYear, color) {
+/*function createObject(first, last, school, gradYear, color) {
   return {
     first: first,
     last: last,
@@ -8,42 +8,46 @@ function createObject(first, last, school, gradYear, color) {
     gradYear: gradYear,
     color: color
   };
+}*/
+function divDOthings(firstname, lastname, pronouns, graduation, favColor) {
+  return (
+    <div>
+      <b>Name: </b>
+      {firstname} {lastname}, <b>pronouns</b>
+      {pronouns}, <b>graduation</b>
+      {graduation}, <b>favorite Color</b>
+      {favColor}
+    </div>
+  );
 }
-
 //{ first + "", last + "", school + "", Gradyear + "", Favcolor + "", FavSocials + "", Favplace + ""}
 
-let classRoster = [
-  createObject("Victor", "Lymar", "LaunchDarkly", "1923", "Green"),
-  createObject("Tracy", "Siebel", "LaunchDarkly", "2022", "Purple"),
-  createObject("Ida", "Voong", "Oakland Technical High School", "2022", "Blue"),
-  createObject("Ana", "Rivera", "CSU East Bay", "2051", "Green"),
-  createObject(
-    "Aiden",
-    "Siebel",
-    "Oakland Technical High School",
-    "2023",
-    "red"
-  ),
-  createObject("Kyle", "wan", "Oakland Tech", "2023", "yeah"),
-  createObject("Enjun", "Li", "John O' Connell", "2022", "black"),
-  createObject("Luis", "Garcia", "CSU East Bay", "2025", "black"),
-  createObject("Armanda", "Yu", "Oakland Technical", "2022", "Blue"),
-  createObject("Yifei", "Du", "Balboa HS", "2022", "Black"),
-  createObject(
-    "Hector",
-    "Del Valle",
-    "John O'Connell High School of Technology ",
-    "2022",
-    "Grey"
-  )
+let hehu = [
+  ["Victor", "Lymar", "1923", "Green"],
+  ["Tracy", "Siebel", "2022", "Purple"],
+  ["Ida", "Voong", "2022", "Blue"],
+  ["Aiden", "Siebel", "2023", "red"],
+  ["Kyle", "wan", "2023", "yeah"],
+  ["Enjun", "Li", "2022", "black"],
+  ["Luis", "Garcia", "2025", "black"],
+  ["Armanda", "Yu", "2022", "Blue"],
+  ["Yifei", "Du", "2022", "Black"],
+  ["Hector", "Del Valle", "2022", "Grey"]
 ];
-console.log(classRoster);
+
+let divs = [];
+for (let i = 0; i < hehu.length; i++) {
+  divs.push(divDOthings(hehu[i]));
+  divs.push(<br />);
+}
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Name</h1>
+
+      <h2>Names</h2>
+      {divs}
     </div>
   );
 }
